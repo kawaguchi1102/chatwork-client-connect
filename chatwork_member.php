85cd0169793bf6f266578b6a37bec4cedf93a750
kawaguchi <info@kawaguchi-design.com>
2019-01-14 12:01:22 +0900

    1st commit

---
+++ chatwork_member.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kawaguchinaoya
+ * Date: 2019-01-14
+ * Time: 02:22
+ */
+//取得したAPIトークンを入れる
+$token = "***************";
+$room_id = "***************";
+
+// ヘッダ
+header("Content-type: text/html; charset=utf-8");
+
+$params = [
+    "body" => "PHPからテスト投稿"
+];
+
+// cURLでPOST
+$ch = curl_init();
+
+$member_get_opt = [
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_URL => "https://api.chatwork.com/v2/rooms/{$room_id}/members",
+    CURLOPT_HTTPHEADER => ['X-ChatWorkToken: '. $token],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+];
+curl_setopt_array( $ch, $member_get_opt );
+
+$rsp = curl_exec($ch);
+curl_close($ch);
+
+// 送信結果も見ておく
+$res = json_decode($rsp);
+
+// json変換
+echo json_encode($res);
---
+++ chatwork_post.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kawaguchinaoya
+ * Date: 2019-01-14
+ * Time: 01:38
+ */
+//取得したAPIトークンを入れる
+$token = "***************";
+$room_id = "***************";
+
+// ヘッダ
+header("Content-type: text/html; charset=utf-8");
+
+$params = [
+    "body" => "PHPからテスト投稿"
+];
+
+// cURLでPOST
+$ch = curl_init();
+
+$post_opt = [
+    CURLOPT_URL => "https://api.chatwork.com/v2/rooms/{$room_id}/messages",
+    CURLOPT_HTTPHEADER => ['X-ChatWorkToken: '. $token],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => http_build_query($params, '', '&')
+];
+curl_setopt_array( $ch, $post_opt );
+
+$rsp = curl_exec($ch);
+curl_close($ch);
+
+// 送信結果も見ておく
+$res = json_decode($rsp);
+var_dump($res);
